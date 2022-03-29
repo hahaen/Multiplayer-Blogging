8d06db348188d365c7c19aea68add88c79292ce5
hahaen <hahaen.love@gmail.com>
2022-03-30 00:01:19 +0800

    初始化

---
+++ src/main/java/com/hahaen/MultiplayerBlogging/MultiplayerBloggingApplication.java
@@ -0,0 +1,13 @@
+package com.hahaen.MultiplayerBlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiplayerBloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiplayerBloggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hahaen/MultiplayerBlogging/MultiplayerBloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahaen.MultiplayerBlogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiplayerBloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
