732887c5c96963994fadb1c8bd593b1e153584b1
hahaen <ihahaen@163.com>
2022-05-18 23:53:57 +0800

    feat(登录)：连接数据库并完成登录模块

--- src/main/java/com/hahaen/MultiplayerBlogging/service/UserService.java
+++ src/main/java/com/hahaen/MultiplayerBlogging/service/UserService.java
@@ -1,6 +1,7 @@
 package com.hahaen.MultiplayerBlogging.service;
 
 import com.hahaen.MultiplayerBlogging.entity.User;
+import com.hahaen.MultiplayerBlogging.mapper.UserMapper;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -9,42 +10,36 @@ import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.util.Collections;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class UserService implements UserDetailsService {
 
-    private Map<String, User> users = new ConcurrentHashMap<>();
-
+    private UserMapper userMapper;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Inject
-    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    public UserService(BCryptPasswordEncoder bCryptPasswordEncoder,
+                       UserMapper userMapper) {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        save("admin", "123456");
+        this.userMapper = userMapper;
+//        save("admin", "123456");
     }
 
     public void save(String username, String password) {
-        users.put(username,
-                new User(1, username, bCryptPasswordEncoder.encode(password)));
+        userMapper.save(username, bCryptPasswordEncoder.encode(password));
     }
 
-//    public User getUserById(Integer id) {
-//        return null;
-//    }
 
     public User getUserByUserName(String username) {
-        return users.get(username);
+        return userMapper.findUserByUsername(username);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if (!users.containsKey(username)) {
+        User user = getUserByUserName(username);
+        if (user == null) {
             throw new UsernameNotFoundException(username + "：用户不存在！");
         }
-
-        User user = users.get(username);
 
         return new org.springframework.security.core.userdetails.User(
                 username, user.getEncryptedPassword(), Collections.emptyList());
