2facaa6adcfb92999e87469f5358d152c88f74a8
hahaen <ihahaen@163.com>
2022-05-29 23:13:04 +0800

    add test

---
+++ src/test/java/com/hahaen/MultiplayerBlogging/service/UserServiceTest.java
@@ -0,0 +1,58 @@
+package com.hahaen.MultiplayerBlogging.service;
+
+import com.hahaen.MultiplayerBlogging.entity.User;
+import com.hahaen.MultiplayerBlogging.mapper.UserMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    BCryptPasswordEncoder mockEncoder;
+    @Mock
+    UserMapper mockUserMapper;
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    void testSave() {
+        when(mockEncoder.encode("myPassword")).thenReturn("myEncodePassword");
+        userService.save("myUsername", "myPassword");
+        Mockito.verify(mockUserMapper).save("myUsername", "myEncodePassword");
+    }
+
+    @Test
+    void testGetUserByUserName() {
+        userService.getUserByUserName("myUsername");
+        Mockito.verify(mockUserMapper).findUserByUsername("myUsername");
+    }
+
+    @Test
+    void throwExceptionWhenUserNotFound() {
+        assertThrows(UsernameNotFoundException.class,
+                () -> userService.loadUserByUsername("myUsername"));
+    }
+
+    @Test
+    void returnUserDetailsWhenUserNotFound() {
+        when(mockUserMapper.findUserByUsername("myUsername"))
+                .thenReturn(new User(123, "myUsername", "myEncodePassword"));
+
+        UserDetails userDetails = userService.loadUserByUsername("myUsername");
+
+        assertEquals("myUsername", userDetails.getUsername());
+        assertEquals("myEncodePassword", userDetails.getPassword());
+    }
+}
