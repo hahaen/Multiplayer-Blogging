ad7c16af722fbdfe475c1e00aac0896b4fda89d6
hahaen <ihahaen@163.com>
2022-06-02 13:04:32 +0800

    添加集成测试

--- src/test/java/com/hahaen/multiplayerblogging/integration/MyIntegrationTest.java
+++ src/test/java/com/hahaen/multiplayerblogging/integration/MyIntegrationTest.java
@@ -1,16 +1,49 @@
 package com.hahaen.multiplayerblogging.integration;
 
 import com.hahaen.multiplayerblogging.MultiplayerBloggingApplication;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = MultiplayerBloggingApplication.class)
-public class MyIntegrationTest {
-    @Test
-    void myIntegrationTest() {
+import javax.inject.Inject;
+import java.io.IOException;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = MultiplayerBloggingApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
+public class MyIntegrationTest {
+
+    @Inject
+    Environment environment;
+
+    @Test
+    void notLoggedInByDefault() throws IOException {
+        String port = environment.getProperty("local.server.port");
+        System.out.println(port);
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpUriRequest request = RequestBuilder.get()
+                .setUri("http://localhost:" + port + "/auth")
+                .build();
+
+        HttpResponse response = httpClient.execute(request);
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        String result = EntityUtils.toString(response.getEntity());
+
+        System.out.println(statusCode);
+        System.out.println(result);
+
+        Assertions.assertEquals(200, statusCode);
+        Assertions.assertTrue(result.contains("用户没有登录"));
     }
 }
